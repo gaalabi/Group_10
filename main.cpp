1f2c2fb465810580b2273f21674819c5b994eff2
tonney7 <30279093+tonney7@users.noreply.github.com>
2018-02-02 15:09:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,7 +30,56 @@ int main(int argc, char* argv[])
 
     outstream.close();
 
-
+    /*
+    
+    Assumming this is how it will look like?
+    
+    database <student> studentfile;
+    bool over = false;
+    int answer;
+    string Reply = "";
+    string Reply_what = "";
+    personnel A;
+    while (!over){
+        cout<<"what would you like to do:"<<endl;
+        cout<<"1: add"<<endl;
+        cout<<"2: find"<<endl;
+        cout<<"3: modify"<<endl;
+        cout<<"4: print"<<endl;
+        cout<<"5: exit"<<endl;
+        cout<<"answer here: ";
+        cin>>answer;
+        
+        switch (answer) {
+            case 1:
+                studentfile.add(A);
+                break;
+            case 2:
+                studentfile.find(A);
+                break;
+            case 3:
+                //if they choose to modify
+                cout<<"what would you like to modify (name,city,YOB,Salary):";
+                cin>>Reply;
+                cout<<endl;
+                cout<<"what would you like to change it to: ";
+                cin>>Reply_what;
+                studentfile.modify(A,Reply,Reply_what);
+                Reply = "";
+                Reply_what = "";
+                break;
+            case 4:
+                studentfile.print();
+                break;
+            case 5:
+                over = true;
+                break;
+            default:
+                break;
+        }
+        cout<<endl<<endl;
+    }
+    */
 
     return 0;
 }
